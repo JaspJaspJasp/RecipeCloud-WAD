b7322b49155445f4a64586730b2f363037b89f13
gisellee616 <giselle.goh.2025@computing.smu.edu.sg>
2026-03-31 19:08:38 +0800

    favourite.js

--- routes/favourites.js
+++ routes/favourites.js
@@ -5,12 +5,14 @@ const favController = require('../controllers/favourites-controller');
 const authentication = require('../middleware/user-auth');
 
 //Render favourites
-router.get('/favourites', authentication.isLoggedIn, favController.getFavourites);
+router.get('/favourites', authentication, favController.getFavourites);
 //Add to database
-router.post('/add', authentication.isLoggedIn, favController.addFavourite);
+router.post('/favourites/add', authentication, favController.addFavourite);
 //
-router.post('/update/:id', authentication.isLoggedIn, favController.updateFavourite);
+router.get('/favourites/:id/edit', authentication, favController.renderEditFavourite)
 //
-router.post('/delete/:id', authentication.isLoggedIn, favController.deleteFavourite);
+router.post('/favourites/:id/edit', authentication, favController.updateFavourite);
+//
+router.post('/favourites/:id/delete', authentication, favController.deleteFavourite);
 
 module.exports = router;
