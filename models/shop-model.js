5147bc1436899201e46986365f448b2de9ea2cce
JasJaspJaspe <zc2cycxcrg@privaterelay.appleid.com>
2026-04-01 01:14:16 +0800

    Update shop-model.js

--- models/shop-model.js
+++ models/shop-model.js
@@ -62,7 +62,7 @@ exports.editItemById = function(userid, updatedList, options = {}) {
 }
 
 exports.deleteItemById = function(userid) {
-    return Shop.deleteOne({userid:userid});   
+    return Shop.deleteOne({ userid:userid });   
 }
 
 
